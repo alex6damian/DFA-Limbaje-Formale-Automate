17f1c7a99cc440067263388047dd470dc139f24e
alex6damian <164188351+alex6damian@users.noreply.github.com>
2024-05-30 18:24:45 +0300

    update

---
+++ emulator.py
@@ -0,0 +1,29 @@
+import dfa_checker
+import cs112_passer
+
+c=cs112_passer.load_file("dfa2.cfg") # citirea fisierului
+
+s=cs112_passer.get_section_list(c) # extragerea sectiunilor
+
+stari=cs112_passer.get_section_content(s, "States") # extragerea starilor
+
+transitions=cs112_passer.get_section_content(s,"Transitions") # extragerea tranzitiilor
+
+curent=cs112_passer.get_section_content(s,'S') # extragerea starii curente
+
+curent=curent[0] # extragerea starii curente
+
+final=cs112_passer.get_section_content(s, 'F') # extragerea starilor finale
+
+string=input() # citirea stringului
+
+for s in string: # parcurgerea stringului
+    for t in transitions: # parcurgerea tranzitiilor
+        if t[0] == curent and s==t[1]: # verificare daca tranzitia este posibila
+            curent=t[2] # schimbarea starii curente
+            break # iesire din for
+
+if curent in final: # verificare daca starea curenta este finala
+    print("Acceptat")  # afisare acceptat
+else: # altfel
+    print("Respins") # afisare respins
