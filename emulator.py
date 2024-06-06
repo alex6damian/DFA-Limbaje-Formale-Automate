c283b780a75927843ca1c0ab697a51a48f5b2dfe
alex6damian <164188351+alex6damian@users.noreply.github.com>
2024-06-06 11:06:57 +0300

    CFG Limbaje Formale si Automate

--- emulator.py
+++ emulator.py
@@ -18,10 +18,14 @@ final=cs112_passer.get_section_content(s, 'F') # extragerea starilor finale
 string=input() # citirea stringului
 
 for s in string: # parcurgerea stringului
+    pasi=0
     for t in transitions: # parcurgerea tranzitiilor
+        pasi=pasi+1
         if t[0] == curent and s==t[1]: # verificare daca tranzitia este posibila
             curent=t[2] # schimbarea starii curente
             break # iesire din for
+    if pasi==len(transitions): # verific daca elementul pe care sunt in string nu se gaseste in tranzitii
+        curent='error'
 
 if curent in final: # verificare daca starea curenta este finala
     print("Acceptat")  # afisare acceptat
